23ed1e057943b5da08fb9003f57432d31a9e9e6c
Daniel Dimitrov <danielddimitrov@outlook.com>
2024-01-26 21:47:55 +0200

    Initial Commit

---
+++ src/main/java/org/auth/multifactor/MultiFactorAuthentication.java
@@ -0,0 +1,11 @@
+package org.auth.multifactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiFactorAuthentication {
+    public static void main(String[] args) {
+        SpringApplication.run(MultiFactorAuthentication.class, args);
+    }
+}
